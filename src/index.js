78dce19aa475769e4383c8b60987ad22e4b58c1a
viviana2397 <vivianaguerracustodio@gmail.com>
2020-02-17 02:22:05 -0500

    last changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 //import validator from './validator.js';
 
 //console.log(validator);//
-//const Validator = document.getElementById('Ncredit');
-//Validator.addEventListener('onclick', Validator ) => { 
- //       alert("El número debe ser de 16 dígitos");
-const validate = document.getElementsByClassName('cardNumber');
-let cardNumber='';
-validate.onclick=function validate(){
-    do{
-        cardNumber=getElementsByClassName('cardNumber').value;
-        if (cardNumber!==''){
-            isValid(cardNumber);
-        }
-        else {
-            alert("Ingresa un número de tajeta válido")
-            document.getElementById('form1').reset();
-        }
-    }
-while (cardNumber==='')
-};
+
+const validar  = document.getElementById('cardN');
+numBer=Array.from(cardN).reverse();
+console.log("number")
